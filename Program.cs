4abd4eeef4c0aa933bbece1ce2ae5467158dfdc7
“Maryam” <maryammaalwahaibi@gmail.com>
2026-01-04 09:12:12 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace CsharpPart1Tasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter how many numbers you need :");
+            int n = int.Parse(Console.ReadLine());
+            int[] numbers = new int[n]; 
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Enter number : ");
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+            int total = 0;
+            for (int i = 0; i < n; i++)
+            {
+                total += numbers[i];
+            }
+            float average = (float)total / n;
+            Console.WriteLine("The average is: " + average);
+
+        }
+    }
+}
