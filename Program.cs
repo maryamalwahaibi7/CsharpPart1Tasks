708fba351dd4dc314e3974285bd1ae071a7a6cd8
“Maryam” <maryammaalwahaibi@gmail.com>
2026-01-04 09:50:00 +0400

    Adding Task 2

--- Program.cs
+++ Program.cs
@@ -4,21 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter how many numbers you need :");
-            int n = int.Parse(Console.ReadLine());
-            int[] numbers = new int[n]; 
-            for (int i = 0; i < n; i++)
+            //Console.WriteLine("Enter how many numbers you need :");
+            //int n = int.Parse(Console.ReadLine());
+            //int[] numbers = new int[n]; 
+            //for (int i = 0; i < n; i++)
+            //{
+            //    Console.WriteLine("Enter number : ");
+            //    numbers[i] = int.Parse(Console.ReadLine());
+            //}
+            //int total = 0;
+            //for (int i = 0; i < n; i++)
+            //{
+            //    total += numbers[i];
+            //}
+            //float average = (float)total / n;
+            //Console.WriteLine("The average is: " + average);
+
+
+            //Task 2
+            Console.WriteLine("Enter a number");
+            float number = float.Parse(Console.ReadLine());
+
+            if (number % 2 == 0)
             {
-                Console.WriteLine("Enter number : ");
-                numbers[i] = int.Parse(Console.ReadLine());
+                Console.WriteLine("The number is Even");
             }
-            int total = 0;
-            for (int i = 0; i < n; i++)
+
+            else
             {
-                total += numbers[i];
+                Console.WriteLine("The number is Odd");
             }
-            float average = (float)total / n;
-            Console.WriteLine("The average is: " + average);
 
         }
     }
