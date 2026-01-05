b096273278d63376ed821e279223a1f994038777
“Maryam” <maryammaalwahaibi@gmail.com>
2026-01-05 08:21:29 +0400

    Adding Task 4

--- Program.cs
+++ Program.cs
@@ -37,33 +37,61 @@
 
 
 
-            //Task 3
-            Console.WriteLine("Enter how many numbers you need :");
+            ////Task 3
+            //Console.WriteLine("Enter how many numbers you need :");
+            //int n = int.Parse(Console.ReadLine());
+
+            //int[] numbers = new int[n];
+
+            //for (int i = 0; i < n; i++)
+            //{
+            //    Console.WriteLine("Enter number : ");
+            //    numbers[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //// Set smallest as the first number in the list
+            //int smallest = numbers[0];
+
+            //// Check each number in the list
+            //for (int i = 1; i < n; i++)
+            //{
+            //    if (numbers[i] < smallest)
+            //    {
+            //        smallest = numbers[i];
+            //    }
+            //}
+
+            //// Output the result
+            //Console.WriteLine("The smallest number is: " + smallest);
+
+
+            //Task 4
+            Console.WriteLine("Enter how many numbers you need:");
             int n = int.Parse(Console.ReadLine());
 
             int[] numbers = new int[n];
 
+            int positiveCount = 0;
+            int negativeCount = 0;
+
             for (int i = 0; i < n; i++)
             {
-                Console.WriteLine("Enter number : ");
+                Console.WriteLine("Enter number:");
                 numbers[i] = int.Parse(Console.ReadLine());
-            }
 
-            // Set smallest as the first number in the list
-            int smallest = numbers[0];
-
-            // Check each number in the list
-            for (int i = 1; i < n; i++)
-            {
-                if (numbers[i] < smallest)
+                if (numbers[i] > 0)
                 {
-                    smallest = numbers[i];
+                    positiveCount++;
+                }
+                else if (numbers[i] < 0)
+                {
+                    negativeCount++;
                 }
             }
 
-            // Output the result
-            Console.WriteLine("The smallest number is: " + smallest);
-
+            // Output the results
+            Console.WriteLine("Number of positive numbers: " + positiveCount);
+            Console.WriteLine("Number of negative numbers: " + negativeCount);
         }
     }
 }
